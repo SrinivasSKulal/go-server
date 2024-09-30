d1c7b21cbe914b747e0fa0615788070589fb247f
Ein-00 <srinivaskulal79@gmail.com>
2024-09-30 18:53:42 +0530

    Updated readme and added images

--- Readme.md
+++ Readme.md
@@ -1,1 +1,8 @@
-"go-server" 
+# Go-Server
+![](mdi/goi.jpg "Hi")
+Example server in go with 2 main pages present in the static folder.
+
+- **index.html**
+	is a welcome page
+- **form.html**
+	has a form that the user can fill.
--- main.go
+++ main.go
@@ -5,41 +5,44 @@ import (
 	"log"
 	"net/http"
 )
-func helloHandler(w http.ResponseWriter , r *http.Request){
-	if r.URL.Path != "/hello"{
-		http.Error(w,"404 not found" , http.StatusNotFound)
-		return 
-	}
-	if r.Method != "GET"{
-		http.Error(w, "Method is not supported" , http.StatusNotFound)
-		return 
-	}
-	fmt.Fprintf(w , "Hello!")
-}
 
-func formHandler(w http.ResponseWriter , r *http.Request){
-	if err := r.ParseForm(); err != nil{
-		fmt.Fprintf(w , "ParseForm() err : %v" , err)
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+	
+		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
-	fmt.Fprintf(w , "POST request successful\n")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-	fmt.Fprintf(w , "Name = %s\n" , name)
-	fmt.Fprintf(w , "Address = %s\n" , address)
-	
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello!")
+
 }
 
-func main(){
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err : %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+
+}
+
+func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/",fileServer)
-	http.HandleFunc("/form",formHandler)
-	http.HandleFunc("/hello",helloHandler)
-	
-	
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
 	fmt.Println("Starting server in port 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil{
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-	
+
 }
+
